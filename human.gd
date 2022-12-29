613d01b7c47b63f4a8bcf7a1136cdece3432c1d0
Marcin Sobczyk <msobczyk@syncad.com>
2022-12-29 18:00:18 +0100

    add file with serveR

---
+++ human.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+
+
+@onready var sprint_arm_pivot = $SpringArmPivot
+@onready var spring_arm = $SpringArmPivot/SpringArm3D
+
+const SPEED = 5.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	
+	if event is InputEventMouseMotion:
+		sprint_arm_pivot.rotate_y(-event.relative.x * .005)
+		spring_arm.rotate_x(-event.relative.y * .005)
+		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
