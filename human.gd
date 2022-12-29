367a110efc4a17ac80372d3e213fd493defea96a
Marcin Sobczyk <msobczyk@syncad.com>
2022-12-29 19:47:33 +0100

    add animation blrend tree for blending animation run speed

--- human.gd
+++ human.gd
@@ -1,10 +1,13 @@
 extends CharacterBody3D
 
 
+@onready var armature = $Armature
 @onready var sprint_arm_pivot = $SpringArmPivot
 @onready var spring_arm = $SpringArmPivot/SpringArm3D
+@onready var anim_tree = $AnimationTree
 
 const SPEED = 5.0
+const LERP_VAL = .15
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -30,11 +33,16 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = direction.rotated(Vector3.UP, sprint_arm_pivot.rotation.y)
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = lerp(velocity.x, direction.x * SPEED, LERP_VAL)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, LERP_VAL)
+		armature.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x, -velocity.z), LERP_VAL)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x, 0.0, LERP_VAL)
+		velocity.z = lerp(velocity.z, 0.0, LERP_VAL)
 
+	anim_tree.set("parameters/BlendSpace1D/blend_position", velocity.length() / SPEED)
 	move_and_slide()
